6791e815c9c19a74cc28c411c0b0339a40d9eba9
Lívia <lligiaoliveira00@gmail>
2025-08-25 16:18:20 -0300

    inicialização das filtragens

--- src/data/dados.js
+++ src/data/dados.js
@@ -1,4 +1,4 @@
-export default [
+const bruxos = [
     {
         id: 1,
         nome: "Harry Potter",
@@ -162,3 +162,112 @@ export default [
 
 
 ]
+
+const varinhas = [
+    [
+        {
+          id: 1,
+          material: "Azevinho",
+          nucleo: "Pena de Fênix",
+          comprimento: "28cm"
+        },
+        {
+          id: 2,
+          material: "Videira",
+          nucleo: "Fibra de Coração de Dragão",
+          comprimento: "27cm"
+        },
+        {
+          id: 3,
+          material: "Salgueiro",
+          nucleo: "Pelo de Unicórnio",
+          comprimento: "35cm"
+        },
+        {
+          id: 4,
+          material: "Carvalho Inglês",
+          nucleo: "Pelo de Testrálio",
+          comprimento: "32cm"
+        }
+      ]
+]
+
+      const animais = [
+    {
+      id: 1,
+      nome: "Coruja",
+      tipo: "Correio/Companhia"
+    },
+    {
+      id: 2,
+      nome: "Gato",
+      tipo: "Companhia"
+    },
+    {
+      id: 3,
+      nome: "Sapo",
+      tipo: "Companhia"
+    },
+    {
+      id: 4,
+      nome: "Rato",
+      tipo: "Companhia"
+    }
+  ]
+
+    
+    const casas = [
+        {
+      id: 1,
+      nome: "Grifinória",
+      fundadora: "Godric Gryffindor",
+      cores: "Vermelho e Dourado",
+      animal: "Leão"
+    },
+    {
+      id: 2,
+      nome: "Sonserina",
+      fundadora: "Salazar Slytherin",
+      cores: "Verde e Prata",
+      animal: "Serpente"
+    },
+    {
+      id: 3,
+      nome: "Corvinal",
+      fundadora: "Rowena Ravenclaw",
+      cores: "Azul e Bronze",
+      animal: "Águia"
+    },
+    {
+      id: 4,
+      nome: "Lufa-Lufa",
+      fundadora: "Helga Hufflepuff",
+      cores: "Amarelo e Preto",
+      animal: "Texugo"
+    }
+  ]
+
+  const pocoes = [
+    {
+      id: 1,
+      nome: "Polissuco",
+      efeito: "Transforma na aparência de outra pessoa"
+    },
+    {
+      id: 2,
+      nome: "Felix Felicis",
+      efeito: "Sorte temporária"
+    },
+    {
+      id: 3,
+      nome: "Amortentia",
+      efeito: "Poção do amor"
+    },
+    {
+      id: 4,
+      nome: "Veritaserum",
+      efeito: "Força a dizer a verdade"
+    }
+  ]
+
+export default { bruxos, varinhas, animais, casas, pocoes} ;
